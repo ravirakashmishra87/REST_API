29f581284033978a87d68116b694d090f4c2ba27
Ravi <raviprakashmishra87@hotmail.com>
2025-12-24 13:34:12 +0530

    Updated Tag_item model to fix issue

--- models/tag_item.py
+++ models/tag_item.py
@@ -2,10 +2,11 @@ from db import db
 
 
 class TagItemModel(db.Model):
-    __tablename__ = "tag_item"
+    __tablename__ = "items_tags"
 
     id = db.Column(db.Integer, primary_key=True)
     tag_id = db.Column(db.Integer, db.ForeignKey("tags.id"))
     item_id = db.Column(db.Integer, db.ForeignKey("items.id"))
-    tag = db.relationship("TagModel", back_populates="tags")
-    items = db.relationship("ItemModel", back_populates="items")
+
+    # tag = db.relationship("TagModel", back_populates="tags")
+    # items = db.relationship("ItemModel", back_populates="items")
